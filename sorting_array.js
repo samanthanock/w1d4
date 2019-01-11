ec51418deb2189f6c8bc2db345e8d65daa9468f2
samanthanock <sammarienock@gmail.com>
2019-01-11 00:19:39 +0000

    fixes sorting problem

--- sorting_array.js
+++ sorting_array.js
@@ -10,24 +10,16 @@ var students = [
   { id: 3, name: 'alex', age: 22 },
   { id: 4, name: 'alex', age: 30 },
 ];
-
-// sort by value
-students.sort(function(a, b) {
-  return a.value - b.value;
-});
-
-// sort by name
-students.sort(function(a, b) {
-  var nameA = a.name.toUpperCase(); // ignore upper and lowercase
-  var nameB = b.name.toUpperCase(); // ignore upper and lowercase
-  if (nameA < nameB) {
+function comparator(x, y) {
+  if (x.name < y.name) {
+    return -10;
+  }
+  if (x.age > y.age) {
     return -1;
   }
-  if (nameA > nameB) {
-    return 1;
-  }
   return 0;
-});
-//Say we have a list of students that we would like to sort in alphabetical order.
-// If they have the same name, then we prioritize the older student first.
-console.log(students.sort());
+}
+
+var result = students.sort(comparator);
+
+console.log(result);
